1822d4ce683ead3cc16105e9383498ffda4d394f
v4litova <152382465+v4litova@users.noreply.github.com>
2023-11-29 14:59:59 +0300

    Update Test.java

--- untitled/src/Practice24/Test.java
+++ untitled/src/Practice24/Test.java
@@ -6,13 +6,14 @@ public class Test {
         Order[] orders = new Order[10];
         for (int i = 0; i < orders.length; i++)
             orders[i] = new RestaurantOrder();
-        orders[3].add(new Dish(287,"Plate", "Now time to check this"));
-        orders[4].add(new Drink(30,"Cup", "It's just for tea"));
-        orders[2].add(new Drink(32,"Cup", "It's just for tea"));
+        orders[3].add(new Dish(287,"Pizza", "Delicious pizza with various toppings"));
+        orders[4].add(new Drink(30,"Tea", "Black tea with a hint of lemon"));
+        orders[2].add(new Drink(32,"Coffee", "Freshly brewed coffee"));
+        
         orderManager.add(orders[3], 7);
         orderManager.add(orders[4], 6);
-        orderManager.add(orders[3],2);
-        orderManager.add("1", orders[3]);
+        orderManager.add(orders[3], 2);
+        orderManager.add("1", orders[4]);
         System.out.println(orderManager.getOrder("1").getItems());
         System.out.println(orderManager.freeTableNumbers());
     }
