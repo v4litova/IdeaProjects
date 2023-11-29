d8be6b51e35685fa76c9d3735df49a7a6ae7ebed
v4litova <152382465+v4litova@users.noreply.github.com>
2023-11-29 14:51:43 +0300

    Update Test.java

--- untitled/src/Practice23/Test.java
+++ untitled/src/Practice23/Test.java
@@ -6,15 +6,18 @@ public class Test {
         Order[] orders = new Order[10];
         for (int i = 0; i < orders.length; i++)
             orders[i] = new Order();
-        orders[3].add(new Dish(287,"Plate", "Now time to check this"));
-        orders[3].add(new Dish(30,"Cup", "It's just for tea"));
-        orders[1].add(new Dish(38, "Clock", "This is not a dish!!!"));
-        orderManager.add(orders[3], 7);
-        orderManager.add(orders[5], 5);
+        
+        orders[3].add(new Dish(287, "Spaghetti", "Delicious pasta with sauce"));
+        orders[3].add(new Dish(30, "Salad", "Fresh green salad"));
+        orders[1].add(new Dish(38, "Steak", "Juicy steak with potatoes"));
+
+        orderManager.add(orders[3], 6);
+        orderManager.add(orders[5], 4);
         orderManager.removeOrder(5);
-        orderManager.add(orders[1],3);
-        System.out.println(orderManager.ordersCostSummary());
-        System.out.println(orderManager.freeTableNumbers());
-        System.out.println(orderManager.freeTableNumber());
+        orderManager.add(orders[1], 3);
+
+        System.out.println("Total cost of all orders: " + orderManager.ordersCostSummary());
+        System.out.println("Free table numbers: " + orderManager.freeTableNumbers());
+        System.out.println("Free table number for a new order: " + orderManager.freeTableNumber());
     }
 }
